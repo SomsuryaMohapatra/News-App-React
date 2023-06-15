332b401249bd7f3be8b9af7770989c09254dbc5d
Somsurya Mohapatra <31140643+SomsuryaMohapatra@users.noreply.github.com>
2023-06-15 11:47:01 +0530

    BootStrap Span(Author , Published Date) , Source Name(Bootstrap Batch)

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 
 export default class NewsItems extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
     return (
       <div>
         <div className="card" style={{ width: "18rem" }}>
+        <span class="position-absolute top-0 translate-middle badge rounded-pill bg-info" style={{left:'80%'}}>
+          {source}
+        </span>
           <img
             src={
               !imageUrl
@@ -18,6 +22,11 @@ export default class NewsItems extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
+            <p className="card-text">
+              <small className="text-body-secondary">
+                By {!author ? "Unknown" : author} on {date}
+              </small>
+            </p>
             <a
               href={newsUrl}
               target="_blank"
